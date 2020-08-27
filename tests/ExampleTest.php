e62973f52c64389f84c5337530402b65a61e70f2
palPalani <palaniappan@innoppl.com>
2020-08-27 22:17:42 +0530

    Added sample tests

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace palPalani\GrumPhpRectorTask\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function true_is_true(): void 
+    {
+        self::assertTrue(true);
+    }
+}
